f6b1c97109184fee556a2aff0379860f56f2c3ab
Eseosa Kay-Uwagboe Pascal <75255976+eseosapku@users.noreply.github.com>
2026-05-28 14:31:02 +0200

    Update PrintReveal.cs
    Print reveal script variables set on trigger enter created

--- Assets/Scripts/PrintReveal.cs
+++ Assets/Scripts/PrintReveal.cs
@@ -1,21 +1,49 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class PrintReveal : MonoBehaviour
 {
+
+
+    private Material printMaterial;
+    private float alphaAmount = 0f;
+    private bool isClose = false;
+    private float printSpeed = 0f;
+    private bool printShown = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        printMaterial = GetComponent<Material>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isClose && printShown is true)
+        {
+
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if 
+        if( other.CompareTag("UV stick"))
+        {
+            isClose = true;
+        }
+    }
+
+    IEnumerator FadePrints()  
+    {
+        float fadeTime = 2f;
+        float elapseTime = 0f;
+
+        while(elapseTime < fadeTime)
+        {
+            elapseTime += Time.deltaTime;
+            float alphaNew = elapseTime / fadeTime;
+        }
     }
 }
