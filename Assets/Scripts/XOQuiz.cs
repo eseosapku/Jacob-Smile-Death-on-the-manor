d754fc32860d565661b4c1c2d3af2f46b005ac24
Eseosa Kay-Uwagboe Pascal <75255976+eseosapku@users.noreply.github.com>
2026-05-29 19:06:32 +0200

    XOquiz check fo answer

--- Assets/Scripts/XOQuiz.cs
+++ Assets/Scripts/XOQuiz.cs
@@ -6,17 +6,64 @@ public class XOQuiz : MonoBehaviour
 {
     public Button xButton;
     public Button oButton;
-    public Image[] statementPanels;
+    public TextMeshProUGUI[] statements;
     public TextMeshProUGUI resultText;
+    private bool[] correctAnswers = { true, false, false, true, false, true, false, true, true };
+    private int currentStatement = 0;
+    private int correctCount = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        xButton.onClick.AddListener(() => AnswerX());
+        oButton.onClick.AddListener(() => AnswerO());
+        HighlightStatement(0);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void AnswerX()
+    {
+        CheckAnswer(false);
+    }
+
+    void AnswerO()
+    {
+        CheckAnswer(true);
+    }
+
+    void CheckAnswer(bool answer)
+    {
+        if (answer == correctAnswers[currentStatement])
+        {
+            statements[currentStatement].color = Color.green;
+            correctCount++;
+
+            if (correctCount == 9)
+            {
+                WinPuzzle();
+            }
+            else
+            {
+                currentStatement++;
+                HighlightStatement(currentStatement);
+            }
+        }
+        else
+        {
+            statements[currentStatement].color = Color.red;
+        }
+    }
+    void HighlightStatement(int index)
+    {
+        statements[index].color = Color.yellow;
+    }
+
+    void WinPuzzle()
+    {
+        resultText.text = "The butler's motive revealed.\nPrrimrose is saved";
     }
 }
