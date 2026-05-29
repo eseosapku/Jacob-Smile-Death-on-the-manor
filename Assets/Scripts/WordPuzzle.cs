5e98e3cceba697c177b42152604664bfea6856f2
Eseosa Kay-Uwagboe Pascal <75255976+eseosapku@users.noreply.github.com>
2026-05-29 15:20:07 +0200

    word puzzle fix

--- Assets/Scripts/WordPuzzle.cs
+++ Assets/Scripts/WordPuzzle.cs
@@ -8,13 +8,14 @@ public class WordPuzzle : MonoBehaviour
 {
     public TextMeshProUGUI arrangementDisplay;
     public Button submitButton;
+    public Image arrangementPanel;
     private string[] correctOrder =
     {
-        "The", "Poison", "is", "inside", "the", "Ice",
-        "not", "in", "the", "milk", "when", "the",
-        "ice", "melted", "the", "poison", "was", "released"
+        "The", "Poison", "is", "inside", "the", "ice",
+        "not", "in", "milk"
     };
     private List<string> playerOrder = new List<string>();
+    private HashSet<string> usedWords = new HashSet<string>();
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -26,18 +27,57 @@ public class WordPuzzle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            RemoveLastWord();
+        }
     }
 
+
+
+    private void ResetPuzzle()
+    {
+        playerOrder.Clear();
+        arrangementDisplay.text = "";
+    }
+
+    private void CheckWordPosition()
+    {
+        int index = playerOrder.Count - 1;
+
+        if (playerOrder[index] != correctOrder[index])
+        {
+            arrangementPanel.color = Color.red;
+        }
+        else
+        {
+            arrangementPanel.color = Color.white;
+        }
+    }
+    private void RemoveLastWord()
+    {
+        if (playerOrder.Count == 0) return;
+        string lastWord = playerOrder[playerOrder.Count - 1];
+        playerOrder.RemoveAt(playerOrder.Count - 1);
+        usedWords.Remove(lastWord);
+        arrangementDisplay.text = string.Join(" ", playerOrder);
+        arrangementPanel.color = Color.white;
+    }
     public void SelectWord(string word)
     {
+        if (usedWords.Contains(word))
+        {
+            Debug.Log("Word already used!");
+            return;
+        }
         playerOrder.Add(word);
+        usedWords.Add(word);
         arrangementDisplay.text = string.Join(" ", playerOrder);
+        CheckWordPosition();
     }
-    void CheckAnswer()
+    private void CheckAnswer()
     {
         bool isCorrect = true;
-
         for (int i = 0; i < correctOrder.Length; i++)
         {
             if (playerOrder[i] != correctOrder[i])
@@ -55,11 +95,5 @@ public class WordPuzzle : MonoBehaviour
             Debug.Log("Wrong order. Try again.");
             ResetPuzzle();
         }
-    }
-
-    void ResetPuzzle()
-    {
-        playerOrder.Clear();
-        arrangementDisplay.text = "";
     }
 }
