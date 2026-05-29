4bf73059c5550151d94446d069b73ddb026484de
Eseosa Kay-Uwagboe Pascal <75255976+eseosapku@users.noreply.github.com>
2026-05-29 16:04:56 +0200

    upfate on the pickup name to use mouse instead of ontrigger enter

--- Assets/Scripts/PickupName.cs
+++ Assets/Scripts/PickupName.cs
@@ -1,16 +1,57 @@
+using TMPro;
 using UnityEngine;
+
 
 public class PickupName : MonoBehaviour
 {
+
+    public TextMeshProUGUI letterDisplay;
+    public TextMeshProUGUI revealText;
+    private string targetName = "JOSEPH";
+    private string collectedLetters = "";
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject == gameObject)
+                {
+                    CollectLetter();
+                }
+            }
+        }
+    }
+
+    void CollectLetter()
+    {
+        string letter = gameObject.name;
+        collectedLetters += letter;
+        letterDisplay.text = collectedLetters;
+
+        gameObject.SetActive(false);
+
+        if (collectedLetters == targetName)
+        {
+            ShowReveal();
+        }
+    }
+
+    void ShowReveal()
+    {
+        revealText.text = "JOSEPH...\n\nThe butler's name is Joseph.\n" +
+                         "Young master Joseph's real name is Jonas.\n\n" +
+                         "The butler is the poisoner.";
     }
 }
